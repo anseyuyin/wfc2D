a45882269f4d34873bc4286dda483db0207969e3
anseyuyin <273280521@qq.com>
2021-08-17 01:33:00 +0800

    更新

--- demos/endlessMap/assets/touchMove.ts
+++ demos/endlessMap/assets/touchMove.ts
@@ -1,9 +1,12 @@
 
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, Component, EventTouch, SystemEventType, Vec3, Vec2 } from 'cc';
+import { TileMap } from './tileMap';
 const { ccclass, property } = _decorator;
 
 @ccclass('TouchMove')
 export class TouchMove extends Component {
+    private static readonly help_v2 = new Vec2();
+    private static readonly help_v3 = new Vec3();
     // [1]
     // dummy = '';
 
@@ -11,12 +14,44 @@ export class TouchMove extends Component {
     // @property
     // serializableDummy = 0;
 
-    start () {
-        // [3]
+    @property({ type: TileMap })
+    tileMapObj: TileMap | null = null;
 
-        this.node.on("touch-start",()=>{
-            console.log(` on touch canvas !`)
+    private startPoint: Vec2 = new Vec2();
+    private startPos: Vec3 = new Vec3();
+
+    start() {
+        // [3]
+        this.node.on(SystemEventType.TOUCH_START, this.onStart, this);
+        this.node.on(SystemEventType.TOUCH_MOVE, this.onMove, this);
+        this.node.on(SystemEventType.TOUCH_END, this.onEnd, this);
+        this.node.on(SystemEventType.TOUCH_CANCEL, (event: EventTouch) => {
+            // console.log(` on touch canvas  TOUCH_CANCEL! touch point : ${event.getLocation().toString()}`)
         });
+    }
+
+    onStart(event: EventTouch) {
+        // console.log(` on touch canvas  start! touch point : ${event.getLocation().toString()}`)
+        event.getLocation(this.startPoint);
+        this.startPos = this.tileMapObj?.node.position.clone() as Vec3;
+
+    }
+
+    onEnd(event: EventTouch) {
+        //复位
+        // this.tileMapObj?.node.setPosition(this.startPos);
+    }
+
+    onMove(event: EventTouch) {
+        // console.log(` on touch canvas  TOUCH_MOVE! touch point : ${event.getLocation().toString()}`)
+        let dtPos = TouchMove.help_v2;
+        event.getLocation(dtPos);
+        dtPos.subtract(this.startPoint);
+        let dtPosV3 = TouchMove.help_v3;
+        dtPosV3.set(dtPos.x, dtPos.y, 0);
+        dtPosV3.add(this.startPos);
+
+        this.tileMapObj?.node.setPosition(dtPosV3);
     }
 
     // update (deltaTime: number) {
